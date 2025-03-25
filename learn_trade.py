c6c801bfe3af88958d3e7dc6dbfeea6f6dd0e336
duokino <75552448+duokino@users.noreply.github.com>
2025-03-25 21:01:25 +0800

    Update learn_trade.py

--- learn_trade.py
+++ learn_trade.py
@@ -17,7 +17,7 @@ colorama.init(autoreset=True)
 mt5.initialize()
 
 # User Input
-symbol = input("Enter currency pair (e.g., EURUSD): ").strip().upper()
+symbol = input("Enter currency pair (e.g., EURUSD, XAUUSD, BTCUSD): ").strip().upper()
 timeframes = {'M1': mt5.TIMEFRAME_M1, 'M5': mt5.TIMEFRAME_M5, 'M15': mt5.TIMEFRAME_M15, 'H1': mt5.TIMEFRAME_H1, 'H4': mt5.TIMEFRAME_H4, 'D1': mt5.TIMEFRAME_D1}
 num_candles = 1000
 
